ddecd017f624581b7b356daecfbd7e6896920c06
Chaitanya Asawa <casawa@stanford.edu>
2018-12-28 14:55:23 -0800

    Created delayedSetText function allowing for a loop to cycle through the text

--- js/index.js
+++ js/index.js
@@ -1,22 +1,42 @@
 var TEXT = [
-    "V E G A N",
-    "If you could reduce suffering, would you?",
+    "If you could reduce suffering",
+    "If you could save 400 lives yearly",
+    "If you could reduce greenhouse emissions",
+    "If you could reduce greenhouse emissions",
+    "Would you?",
+    "Now imagine this was all done to humans",
     "I'm not going to say it's super easy - but it is incredibly \
      gratifying knowing every meal is suffering-free",
+    "All it takes is a choice",
+    "V E G A N",
 ];
 
 function setText(elem, text) {
     /* Sets the text of a jQuery element.
      *
      * Parameters
-     * -----------
+     * ----------
      * elem : jQuery element
      * text : string
      */
     elem.text(text);
 }
 
+function delayedSetText(elem, text, delay) {
+    /* Sets the text of a jQuery element after
+     * delay ms.
+     *
+     * Parameters
+     * ----------
+     * elem : jQuery element
+     * text : string
+     * delay : int
+     *   Delay in ms
+     */
+    setTimeout(function() { setText(elem, text); }, delay);
+}
+
 // Change the text over time
-setText($("#main_text"), TEXT[0]);
-setTimeout(function() { setText($("#main_text"), TEXT[1]); }, 2000);
-setTimeout(function() { setText($("#main_text"), TEXT[2]); }, 4000);
+for (var i = 0; i < TEXT.length; i++) {
+    delayedSetText($("#main_text"), TEXT[i], i * 2000);
+}
