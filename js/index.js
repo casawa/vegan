05f48d37cf0cebb3236228b079087e0d14f6feed
Chaitanya Asawa <casawa@stanford.edu>
2018-12-27 23:59:35 -0800

    Add some more text and cycle through the text over time

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 var TEXT = [
     "V E G A N",
     "If you could reduce suffering, would you?",
-]
+    "I'm not going to say it's super easy - but it is incredibly \
+     gratifying knowing every meal is suffering-free",
+];
 
 function setText(elem, text) {
     /* Sets the text of a jQuery element.
@@ -14,4 +16,7 @@ function setText(elem, text) {
     elem.text(text);
 }
 
-setText($("#main_text"), TEXT[0])
+// Change the text over time
+setText($("#main_text"), TEXT[0]);
+setTimeout(function() { setText($("#main_text"), TEXT[1]); }, 2000);
+setTimeout(function() { setText($("#main_text"), TEXT[2]); }, 4000);
